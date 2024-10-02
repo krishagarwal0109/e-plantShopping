c2a83782f5ed134095932fe2d64bd25156c3627b
Krish Agarwal <krish.agarwal.ece23@itbhu.ac.in>
2024-10-02 08:59:45 +0530

    update cartslice.jsx to add additem,removeitem and updatequantity functions

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,25 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+      const {name,image,description,cost}=action.payload;
+      const itemfound= state.items.find(item=>item.name==name);
+      if(itemfound){
+        itemfound.count++;
+      }  
+      else{
+        state.items.push({name,image,description,cost,count:1});
+      }
+      },
     
-    },
     removeItem: (state, action) => {
+      state.items=state.items.filter(item=>action.payload!=item.name);
     },
     updateQuantity: (state, action) => {
-
+      const {name,count}=action.payload;
+      const itemtomodify=state.items.find(item=>item.name==name);
+      if(itemtomodify){
+        itemtomodify.count=count;
+      }
     
     },
   },
